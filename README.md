8a6bf298cef4e890066d7bb48dd9401cb4357e01
Bagohart <bagohart@gmx.de>
2023-09-14 13:20:09 +0200

    rewrote normal mode mappings to avoid dependency on vim-repeat

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # git-rebase-mappings.nvim
 Neovim plugin that adds some simple mappings to make git-rebase more fun.
+All mappings are dot-repeatable, and do not require [vim-repeat](https://github.com/tpope/vim-repeat).
 
 ## Mappings
 No default mappings, create them explicitly:
@@ -10,12 +11,12 @@ require('git-rebase-mappings').create_commit_command_mappings()
 ```
 
 The function `create_commit_command_mappings(prefix)` creates the following mappings:
-- `<LocalLeader>p`, `<LocalLeader>P`
-- `<LocalLeader>r`, `<LocalLeader>R`
-- `<LocalLeader>e`, `<LocalLeader>E`
-- `<LocalLeader>s`, `<LocalLeader>S`
-- `<LocalLeader>f`, `<LocalLeader>F`
-- `<LocalLeader>d`, `<LocalLeader>D`
+- `<LocalLeader>p`, `<LocalLeader>P` (pick)
+- `<LocalLeader>r`, `<LocalLeader>R` (reword)
+- `<LocalLeader>e`, `<LocalLeader>E` (edit)
+- `<LocalLeader>s`, `<LocalLeader>S` (squash)
+- `<LocalLeader>f`, `<LocalLeader>F` (fixup)
+- `<LocalLeader>d`, `<LocalLeader>D` (drop)
 
 It accepts an optional parameter to change the default prefix from `<LocalLeader>` to something else, e.g. `<Leader>`.
 
@@ -27,5 +28,4 @@ An operator is not provided for this, since the usual case is to change only the
 Other git-rebase commands such as `exec` are not added for this kind of maping since they don't make sense in this context.
 
 ## Requirements
-Developed and tested on Neovim `0.8.1`.  
-Requires [vim-repeat](https://github.com/tpope/vim-repeat).
+Developed and tested on Neovim `0.9.1`.
--- lua/git-rebase-mappings.lua
+++ lua/git-rebase-mappings.lua
@@ -1,13 +1,29 @@
+vim.cmd([[
+    " the global scope here is ugly, but putting it in script scope (:s) means the function is not available from init.lua
+    " so I don't currently have a better solution for this
+    function! Git_rebase_mappings_commit_command(cmd)
+        function! s:inner(...) closure abort
+            execute 'normal! ^WWBBciw' . a:cmd . "\<Esc>W"
+        endfunction
+        let &opfunc=get(funcref('s:inner'), 'name')
+        return 'g@l'
+    endfunction
+]])
+
 local function create_commit_command_mappings(prefix)
     prefix = prefix or '<LocalLeader>'
     local commitCommandsShort = { p = 'p', r = 'r', e = 'e', s = 's', f = 'f', d = 'd' }
     for key, val in pairs(commitCommandsShort) do
         local opt = { buffer = true, desc = "Change current command to '" .. val .. "'", silent = true }
-        vim.keymap.set(
-            'n',
-            '<Plug>(git-rebase-mappings-n-use-command-' .. val .. ')',
-            '^WWBBciw' .. val .. '<Esc>W' .. [[:silent call repeat#set("\<Plug>(git-rebase-mappings-n-use-command-]] .. val .. ')")<CR>',
-            opt
+        -- This worked for using it with vim-repeat (, but I wanted to get rid of that):
+        -- vim.keymap.set(
+        --     'n',
+        --     '<Plug>(git-rebase-mappings-n-use-command-' .. val .. ')',
+        --     '^WWBBciw' .. val .. '<Esc>W' .. [[:silent call repeat#set("\<Plug>(git-rebase-mappings-n-use-command-]] .. val .. ')")<CR>',
+        --     opt
+        -- )
+        vim.cmd(
+            'nnoremap <silent><expr><buffer> <Plug>(git-rebase-mappings-n-use-command-' .. val .. ') Git_rebase_mappings_commit_command("' .. val .. '")'
         )
         vim.keymap.set('x', '<Plug>(git-rebase-mappings-x-use-command-' .. val .. ')', ':normal! ^WWBBciw' .. val .. '<Esc><CR>`<', opt)
         -- the WWBB aborts the command if not enough text is there. that's a very simple detection if the command shouldn't be run on this line.
@@ -18,11 +34,15 @@ local function create_commit_command_mappings(prefix)
     local commitCommandsLong = { P = 'pick', R = 'reword', E = 'edit', S = 'squash', F = 'fixup', D = 'drop' }
     for key, val in pairs(commitCommandsLong) do
         local opt = { buffer = true, desc = "Change current command to '" .. val .. "'", silent = true }
-        vim.keymap.set(
-            'n',
-            '<Plug>(git-rebase-mappings-n-use-command-' .. val .. ')',
-            '^WWBBciw' .. val .. '<Esc>W' .. [[:silent call repeat#set("\<Plug>(git-rebase-mappings-n-use-command-]] .. val .. ')")<CR>',
-            opt
+        -- This worked for using it with vim-repeat (, but I wanted to get rid of that):
+        -- vim.keymap.set(
+        --     'n',
+        --     '<Plug>(git-rebase-mappings-n-use-command-' .. val .. ')',
+        --     '^WWBBciw' .. val .. '<Esc>W' .. [[:silent call repeat#set("\<Plug>(git-rebase-mappings-n-use-command-]] .. val .. ')")<CR>',
+        --     opt
+        -- )
+        vim.cmd(
+            'nnoremap <silent><expr><buffer> <Plug>(git-rebase-mappings-n-use-command-' .. val .. ') Git_rebase_mappings_commit_command("' .. val .. '")'
         )
         vim.keymap.set('x', '<Plug>(git-rebase-mappings-x-use-command-' .. val .. ')', ':normal! ^WWBBciw' .. val .. '<Esc><CR>`<', opt)
         vim.keymap.set('n', prefix .. key, '<Plug>(git-rebase-mappings-n-use-command-' .. val .. ')', { buffer = true })
